839d7dbfc1799a8a8f48c94e064cdc7b837787c1
unknown <dudtjr913@naver.com>
2020-08-23 18:44:16 +0900

    success

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import * as sound from "./sound.js";
 
 const popUp = new PopUpContainer();
 const gamePlay = new GameBuilder()
-  .duration(3)
-  .carrotCount(5)
-  .bugCount(5)
+  .duration(10)
+  .carrotCount(10)
+  .bugCount(10)
   .build();
 
 gamePlay.setOnClickListener((win) => {
